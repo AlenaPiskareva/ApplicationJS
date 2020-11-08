f9b6aae74dcbeb5e77872782bdbf4f4153d7d976
Alena <a.piskareva98@yandex.ru>
2020-11-08 19:36:18 +0300

    Task 2. Добавление комментариев

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 //document.querySelector('.page').classList.remove('second-page');
+//объвление переменной
 let page = document.querySelector('.page');
 page.classList.remove('second-page');
+
+//обработчик событий
 let lightButton = document.querySelector('.light-button');
 lightButton.onclick = function(){
     console.log('Кнопка нажата!');
 }
+//Вывод в консоль текстового содержимого для .email-subtitle
 let message = document.querySelector('.email-subtitle');
 console.log(message.textContent);
