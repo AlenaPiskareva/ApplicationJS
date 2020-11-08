b86f8024935536209f70d3aa338e07823b5f712f
Alena <a.piskareva98@yandex.ru>
2020-11-08 15:54:17 +0300

    Task 2. 1. объявила переменную lightButton, 2.добавила обработчик событий, 3. добавила фразу для вывода в консоли

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 //document.querySelector('.page').classList.remove('second-page');
 let page = document.querySelector('.page');
 page.classList.remove('second-page');
+let lightButton = document.querySelector('.light-button');
+lightButton.onclick = function(){
+    console.log('Кнопка нажата!');
+}
