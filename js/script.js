3841f79d45266fdad5be8d3b3a066f5e20aa48e0
Alena <a.piskareva98@yandex.ru>
2020-11-11 18:09:50 +0300

    Task3. 1) Получаем данные из поля ввода

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 //document.querySelector('.page').classList.remove('second-page');
 //объвление переменной
-let page = document.querySelector('.page');
-page.classList.remove('second-page');
+//let page = document.querySelector('.page');
+//page.classList.remove('second-page');
 
 //обработчик событий
-let lightButton = document.querySelector('.light-button');
-lightButton.onclick = function(){
-    console.log('Кнопка нажата!');
-}
+//let lightButton = document.querySelector('.light-button');
+//lightButton.onclick = function(){
+    //console.log('Кнопка нажата!');
+//}
 //Вывод в консоль текстового содержимого для .email-subtitle
-let message = document.querySelector('.email-subtitle');
-console.log(message.textContent);
+//let message = document.querySelector('.email-subtitle');
+//console.log(message.textContent);
+
+//Task3
+//С помощью свойства value получаем данные из поля ввода
+let input = document.querySelector('input');
+console.log(input.value);
