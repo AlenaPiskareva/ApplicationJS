82012dfdecd5c6f3535d1deff647bcd8a770d99e
Alena <a.piskareva98@yandex.ru>
2020-11-18 13:55:48 +0300

    Task5. Решение задачи №1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-document.querySelector('.page').classList.remove('second-page');
+
+
+//Task 5
+//Идентификаторы
+let userName = {
+    name: "Alena",
+    }
+const userYear = {
+    year: 1998,
+    }
+alert (`Привет, ${userName.name}`);
