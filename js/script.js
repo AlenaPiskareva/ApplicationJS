03aaec9dba768e6f39f47be59336a95abc6b2480
Alena <a.piskareva98@yandex.ru>
2020-11-24 14:35:28 +0300

    Task 5. 1) Решение задачи "Циклы" с помощью понятие "массив" через метод push. 2) Решение задачи "Циклы" без понятия "массив". 3) Решение задач "Функции"

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+'use strict';
 
 //Task 5
 //Идентификаторы
@@ -14,6 +14,7 @@ alert (`Привет, ${userName.name}`);
 messageName();*/
 
 //Циклы
+//Без массива
 /*function range(){
    let start = 15;
    let end = 30;
@@ -21,8 +22,18 @@ messageName();*/
     alert (array);
 }
 range();*/
+//С массивом через push
+/*function range(start, end){
+    let array = [];
+    for (let i = 15; i <= 30; i++){
+        array.push(i)
+    }
+    return(array)
+}
+alert(range());*/
 
-function rangeOdd(){
+//Без массива
+/*function rangeOdd(){
     let start = 15;
     let end = 30;
     for (let array = start; array <= end; array++){
@@ -31,4 +42,58 @@ function rangeOdd(){
         }
     }
 }
-rangeOdd();
+rangeOdd();*/
+//С массивом через push
+/*function rangeOdd(start, end){
+    let array = [];
+    for (let i = 15; i <= 30; i++){
+        if (i % 2 != 0){
+            array.push(i)
+        }
+    }
+    return(array)
+}
+alert(rangeOdd());*/
+
+//Функции
+/*let a = prompt('Введите первое число:','' );
+let b = prompt('Введите второе число', '');
+function average(a, b){
+    return (Number(a) + Number(b)) / 2;
+}
+let result = average(a, b);
+alert(result);*/
+
+/*let x = prompt('Введите основание степени:','' );
+let number = 2;
+function square(x){
+    return(Math.pow(x,number));
+}
+let result = square(x);
+alert(result);*/
+
+/*let x = prompt('Введите основание степени:','' );
+let number = 3;
+function cube(x){
+    return(Math.pow(x,number));
+}
+let result = cube(x);
+alert(result);*/
+
+/*function square(x){
+    return(x ** 2);
+}
+function cube(x){
+    return(x ** 3);
+}
+function average(a, b){
+    return (a + b) / 2;
+}
+function calculate(){
+    let arr = [];
+    for (let i = 0; i <= 9; i++) {
+        arr.push(average(square(i),cube(i)));
+    }
+    return(arr);
+}
+alert(calculate());*/
