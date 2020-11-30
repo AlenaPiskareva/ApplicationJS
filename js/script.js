aa2698ee3d25b42bf4c8fee031d0661e4a62924e
Alena <a.piskareva98@yandex.ru>
2020-11-30 12:00:27 +0300

    Task6. Решение задачи №1.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 'use strict'
+
+//Task 6.
+//Задача 1.
+/*let result = prompt('Введите число:', '');
+function factorial(n){
+    return(result);
+}
+    for (let i = 1; i < result; i++){
+    result = Number(result) * i;
+    }
+    
+alert(factorial(n));*/
+let n = prompt('Введите число:', '');
+let result = 1;
+function factorial(n){
+    return(result);
+}
+for (let i = 1; i <= n; ++i){
+result = result * i;
+}
+
+alert(factorial(n));
